bb04562fef8c8516106fd5dda646b4cc6255a015
Tanya Agarwal <jiya03tanya@gmail.com>
2024-08-15 18:37:11 +0530

    added frontend UI for dashboard, course and catalog

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
-import {Route, Routes } from "react-router-dom";
+import {Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home"
 import Navbar from "./components/common/Navbar"
-import About from "./pages/About";
-
 import OpenRoute from "./components/core/Auth/OpenRoute"
 
 import Login from "./pages/Login"
@@ -11,7 +9,7 @@ import Signup from "./pages/Signup"
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
-
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 import MyProfile from "./components/core/Dashboard/MyProfile";
 import Dashboard from "./pages/Dashboard";
@@ -22,12 +20,14 @@ import { useDispatch, useSelector } from "react-redux";
 import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
 import Cart from "./components/core/Dashboard/Cart";
 import { ACCOUNT_TYPE } from "./utils/constants";
-
 import AddCourse from "./components/core/Dashboard/AddCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
 import EditCourse from "./components/core/Dashboard/EditCourse";
 import Catalog from "./pages/Catalog";
-
+import CourseDetails from "./pages/CourseDetails";
+import ViewCourse from "./pages/ViewCourse";
+import VideoDetails from "./components/core/ViewCourse/VideoDetails";
+import Instructor from "./components/core/Dashboard/InstructorDashboard/Instructor";
 
 function App() {
 
@@ -38,12 +38,13 @@ function App() {
 
 
   return (
-    // set width to screen size by w-screen
-    // min-h-screen used to make an element span the entire height of the viewport minimum
    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="catalog/:catalogName" element={<Catalog/>} />
+      <Route path="courses/:courseId" element={<CourseDetails/>} />
+      
       <Route
           path="signup"
           element={
@@ -89,11 +90,11 @@ function App() {
         />  
 
     <Route
-          path="about"
+          path="/about"
           element={
-            <OpenRoute>
+            
               <About />
-            </OpenRoute>
+            
           }
         />
     <Route path="/contact" element={<Contact />} />
@@ -103,9 +104,10 @@ function App() {
         <PrivateRoute>
           <Dashboard />
         </PrivateRoute>
-      } 
+      }
     >
       <Route path="dashboard/my-profile" element={<MyProfile />} />
+      
       <Route path="dashboard/Settings" element={<Settings />} />
       
 
@@ -118,10 +120,10 @@ function App() {
         )
       }
 
-      {/* if account type is INSTRUCTOR then add :- add-course, my-courses, edit-course in dashboard */}
       {
         user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
           <>
+          <Route path="dashboard/instructor" element={<Instructor />} />
           <Route path="dashboard/add-course" element={<AddCourse />} />
           <Route path="dashboard/my-courses" element={<MyCourses />} />
           <Route path="dashboard/edit-course/:courseId" element={<EditCourse />} />
@@ -134,6 +136,26 @@ function App() {
     </Route>
 
     
+      <Route element={
+        <PrivateRoute>
+          <ViewCourse />
+        </PrivateRoute>
+      }>
+
+      {
+        user?.accountType === ACCOUNT_TYPE.STUDENT && (
+          <>
+          <Route 
+            path="view-course/:courseId/section/:sectionId/sub-section/:subSectionId"
+            element={<VideoDetails />}
+          />
+          </>
+        )
+      }
+
+      </Route>
+
+
 
     <Route path="*" element={<Error />} />
 
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,191 +1,149 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from "react"
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
+import { BsChevronDown } from "react-icons/bs"
+import { useSelector } from "react-redux"
+import { Link, matchPath, useLocation } from "react-router-dom"
+
 import logo from "../../assets/Logo/Logo-Full-Light.png"
-import { Link, matchPath } from 'react-router-dom'
-import {NavbarLinks} from "../../data/navbar-links"
+import { NavbarLinks } from "../../data/navbar-links"
+import { apiConnector } from "../../services/apiconnector"
+import { categories } from "../../services/apis"
+import { ACCOUNT_TYPE } from "../../utils/constants"
+import ProfileDropdown from "../core/Auth/ProfileDropDown"
 
-import { useLocation } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+function Navbar() {
+  const { token } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.profile)
+  const { totalItems } = useSelector((state) => state.cart)
+  const location = useLocation()
 
-import {AiOutlineShoppingCart} from "react-icons/ai"
+  const [subLinks, setSubLinks] = useState([])
+  const [loading, setLoading] = useState(false)
 
-import ProfileDropDown from '../core/Auth/ProfileDropDown'
-import { apiConnector } from '../../services/apiconnector'
-import { categories } from '../../services/apis'
-import { useState } from 'react'
-import {IoIosArrowDropdownCircle} from "react-icons/io"
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        const res = await apiConnector("GET", categories.CATEGORIES_API)
+        setSubLinks(res.data.data)
+      } catch (error) {
+        console.log("Could not fetch Categories.", error)
+      }
+      setLoading(false)
+    })()
+  }, [])
 
-const subLinks = [
-    {
-        title: "python",
-        link:"/catalog/python"
-    },
-    {
-        title: "web dev", 
-        link:"/catalog/web-development"
-    },
-];
+  // console.log("sub links", subLinks)
 
-
-const Navbar = () => {
-    console.log("Printing base url: ",process.env.REACT_APP_BASE_URL);
-
-    // destructuring:- authSlice se state nikaal rhe
-    const {token} = useSelector( (state) => state.auth );
-    const {user} = useSelector( (state) => state.profile ); 
-    const {totalItems} = useSelector( (state) => state.cart )  
-
-    const location = useLocation();
-
-    const [ssubLinks, setSsubLinks]  = useState([]);
-
-    const fetchSublinks = async() => {
-        try{
-            // GET kro showAllCategories k url ko jo stored h in categories_api in categories
-            const result = await apiConnector("GET", categories.CATEGORIES_API);
-            console.log("Printing Sublinks result:" , result);
-            // saari categories ki list ko sublinks me store krna h taki ye catalog m display ho
-            setSsubLinks(result.data.data);
-        }  
-        catch(error) {
-            console.log("Could not fetch the category list");
-        }
-    }
-
-
-    // fetch all sublinks
-    useEffect( () => {
-        console.log("PRINTING TOKEN", token);
-        fetchSublinks();
-    },[] )
-
-
-
-    const matchRoute = (route) => {
-        // is location ko access krne k liye , useLocation likho
-        return matchPath({path:route}, location.pathname);
-    }
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname)
+  }
 
   return (
-    <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
-        <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
-
-            {/* Image */}
-            <Link to="/">
-                <img src={logo} width={160} height={42} loading='lazy'/>
+    <div
+      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+        location.pathname !== "/" ? "bg-richblack-800" : ""
+      } transition-all duration-200`}
+    >
+      <div className="flex w-11/12 max-w-maxContent items-center justify-between">
+        {/* Logo */}
+        <Link to="/">
+          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
+        </Link>
+        {/* Navigation links */}
+        <nav className="hidden md:block">
+          <ul className="flex gap-x-6 text-richblack-25">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks.length ? (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses?.length > 0
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute( )
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        {/* Login / Signup / Dashboard */}
+        <div className="hidden items-center gap-x-4 md:flex">
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
             </Link>
-
-            {/* Nav Links */}
-            <nav>
-                <ul className='flex gap-x-6 text-richblack-25'>
-                {
-                    NavbarLinks.map( (link, index) => (
-                        <li key={index}>
-                            {
-                                // if catalog then handle differently else create a link
-                                link.title === "Catalog" ? (
-
-                                    <div className='relative flex items-center gap-2 group'>
-
-                                        {/* title k baad down arrow lgaya */}
-                                        <p>{link.title}</p>
-                                        <IoIosArrowDropdownCircle/>
-
-                                        {/* ab hover krne me jo dropdown aarha, vo banayege */}
-                                        <div className='invisible absolute left-[50%] 
-                                        translate-x-[-50%] translate-y-[80%] top-[50%]
-                                        flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900
-                                        opacity-0 transition-all duration-200 group-hover:visible
-                                        group-hover:opacity-100 lg:w-[300px]'>
-
-                                            {/* triangle type ki figure is done by rotating rectangle by 45 degree */}
-                                            <div className='absolute left-[50%] top-0 translate-x-[80%]
-                                            translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5'>
-                                            </div> 
-
-                                            {
-                                                // non empty set of sublinks
-                                                subLinks.length ? (
-                                                        subLinks.map( (subLink, index) => (
-                                                            <Link to={`${subLink.link}`} key={index}>
-                                                                <p>{subLink.title}</p>
-                                                            </Link>
-                                                        ) )
-                                                ) : (<div></div>)
-                                            }
-
-                                        </div>
-
-                                    </div>
-
-                                ) : (
-                                    // create a link if not catalog 
-                                    <Link to={link?.path}>
-                                        {/* agr koi navlink khuli h then its text is shown with yellow color */}
-                                        {/* means jo hm path bhej rhe or current url agr match hai , then color it yellow */}
-                                        <p className={`${ matchRoute(link?.path) ? "text-yellow-25" : "text-richblack-25"}`}>
-                                            {link.title}
-                                        </p> 
-                                    </Link>
-                                )
-                            }
-                        </li>
-                    ) )
-                }
-                </ul>
-            </nav>
-
-
-            {/* Login/SignUp/Dashboard */}
-            <div className='flex gap-x-4 items-center'>
-
-                {/* cart display on navbar */}
-                    {
-                        // if user is not instructor and cart pe click krte hi vo cart pe jaaye
-                        user && user?.accountType != "Instructor" && (
-                            <Link to="/dashboard/cart" className='relative'>
-                                <AiOutlineShoppingCart />
-                                {
-                                    // if totalItems >0 then display tototal no. of items
-                                    totalItems > 0 && (
-                                        <span>
-                                            {totalItems}
-                                        </span>
-                                    )
-                                }
-                            </Link>
-                        )
-                    }
-
-                {/* login/signup ya phr user vali cheeze display on navbar */}
-                    {/* ab ye decide krege ki login butto dikhana h ya ni on navbar */}
-                    {
-                        token === null && (
-                            <Link to="/login">
-                                <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
-                                    Log in
-                                </button>
-                            </Link>
-                        ) 
-                    }
-                    {
-                        token === null && (
-                            <Link to="/signup">
-                                <button  className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
-                                    Sign Up
-                                </button>
-                            </Link>
-                        )
-                    }
-
-                    {/* is case me user not null mtlb user exist krta hai, to uska icon , dropdown menu vgera dikhao */}
-                    {
-                        token !== null && <ProfileDropDown />
-                    }
-                
-            </div>
-
-
+          )}
+          {token === null && (
+            <Link to="/login">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Sign up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
         </div>
+        <button className="mr-4 md:hidden">
+          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+        </button>
+      </div>
     </div>
   )
 }
